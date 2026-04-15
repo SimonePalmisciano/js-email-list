aab960a2fd7761713659178ba88de09c73259ff8
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-15 15:47:10 +0200

    aggiunto listener al btn e creata funzione con ciclo for per generare 10 mail

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,13 @@
 // Seleziono gli elementi dal DOM
 const generateBtn = document.querySelector('#generate-btn');
 const emailList = document.querySelector('#email-list');
+
+generateBtn.addEventListener('click', generateMail);
+
+// funzione che genera le 10 mail
+function generateMail () {
+    // Ciclo per fare 10 richieste di generazione mail
+    for (let i = 0; i < 10; i++) {
+
+    }
+};
