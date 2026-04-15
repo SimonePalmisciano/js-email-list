8bb43c250a281bfeb251ec4e084649c7fe1f4b16
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-15 15:51:49 +0200

    collegato il server con fetch e il primo .then

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,17 @@ const emailList = document.querySelector('#email-list');
 
 generateBtn.addEventListener('click', generateMail);
 
-// funzione che genera le 10 mail
+// funzione che genererà 10 mail al clicl del btn
 function generateMail () {
     // Ciclo per fare 10 richieste di generazione mail
     for (let i = 0; i < 10; i++) {
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail') // faccio una richiesta al server
+            .then(function (response) {
+                
+                // Converto la risposta in JSON e la mando al successivo .then
+                return response.json();
+                
+            })
 
     }
 };
